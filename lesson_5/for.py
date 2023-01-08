caf0a8ea04ab4f53d236989909e93df5782b223b
Angeles <angelessevilla18@gmail.com>
2023-01-08 20:15:54 +0100

    add lesson_5 for loop

---
+++ lesson_5/for.py
@@ -0,0 +1,13 @@
+# for contador in range(1, 1001):
+#     print(contador)
+
+
+# for i in range(11):
+#     print(11 * i)
+
+
+for i in range(10):
+    print(f"Esta es la tabla del {i}")
+    for j in range(10):
+        print(f"{i} x {j} = {i*j}")
+         
