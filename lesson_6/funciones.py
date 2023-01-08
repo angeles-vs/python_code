e1ee0d5765ab4433ea62668f6e8f6010d2a86bbc
Angeles <angelessevilla18@gmail.com>
2023-01-08 20:17:24 +0100

    add lesson_6 funciones

---
+++ lesson_6/funciones.py
@@ -0,0 +1,20 @@
+def conversacion(mensaje):
+    print("Hola")
+    print("Como estas?")
+    print(mensaje)
+    print("Adios")
+
+
+opcion = int(input("Elige una opcion (1,2,3):"))
+if opcion == 1:
+    conversacion ("Comida forever")
+
+elif opcion == 1:
+    conversacion ("Comida never")
+
+elif opcion == 1:
+    conversacion ("Comida always") 
+
+else:
+   print("Escribe la opcion correcta...")
+
